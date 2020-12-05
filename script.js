b1a0f01ad493879f052767cbbf23ef8bab5ea0e8
EgorDed <69810825+EgorDed@users.noreply.github.com>
2020-12-05 12:02:02 +0200

    Add files via upload

--- script.js
+++ script.js
@@ -1,16 +1,26 @@
-var img = document.querySelectorAll('.item_img');
+var nav = document.querySelector('.menu');
+var burger = document.querySelector('.burger');
 
-var src = img[0].attributes.src.value;
+document.addEventListener("DOMContentLoaded", function(){
+    nav.classList.add('anim-disabled');
+    burger.classList.add('anim-disabled');
+});
 
-for (i = 0; i < img.length; i++) {
-    img[i].addEventListener('mouseenter', function(e) {
-        var newsrc = e.target.attributes.newsrc.value;
-        e.target.attributes.src.value = newsrc;
-    })
+window.addEventListener('resize', function(){
+   if (nav.classList.contains('menu-hide') && burger.classList.contains('burger-position')) {
+       nav.classList.add('anim-disabled');
+       burger.classList.add('anim-disabled');
 }
 
-for (i = 0; i < img.length; i++) {
-        img[i].addEventListener('mouseleave', function(e) {
-        e.target.attributes.src.value = src;
-    })
-}
+});
+
+burger.addEventListener('click', function () {
+    nav.classList.toggle('menu-hide');
+    nav.classList.remove('anim-disabled');
+
+    burger.classList.toggle('burger-position');
+    burger.classList.toggle('burger-anim-in')
+    burger.classList.remove('anim-disabled');
+
+    burger.classList.toggle('burger-mark');
+});
